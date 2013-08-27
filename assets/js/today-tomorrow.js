7ab6a20f5cccc6fc1554ecd399d61dde5f061d0f
Giovanni Ferron <gvnnrulez@gmail.com>
2013-08-27 22:24:11 +1000

    Delete, Done and Write command added to translations

--- assets/js/today-tomorrow.js
+++ assets/js/today-tomorrow.js
@@ -11,7 +11,7 @@
                             '</div>' +
                             '{{#.editing}}' +
                             '<div proxy-click="unedit" class="edit-container">' +
-                                ' <a proxy-click="complete:{{i}}">{{( completed ? \'Not Done\' : \'Done\' )}}</a>' +
+                                ' <a proxy-click="complete:{{i}}">{{{( completed ? lang.cmdNotDone : lang.cmdDone )}}}</a>' +
                                 ' <a proxy-click="move:{{( date == todayDate() ? \'tomorrow\' : \'today\' )}}">{{{( date == todayDate() ? lang.tomorrow : lang.today )}}}</a>' +
                                 ' <a proxy-click="forget:{{i}}">{{lang.cmdDelete}}</a>' +
                             '</div>' +
@@ -19,60 +19,60 @@
                         '</div></li>';
 
     var translations = {
-        en: { code: "en", name: "English", today: "Today", tomorrow: "Tomorrow", cmdDelete: "Delete" },
-        af: { code: "af", name: "Afrikaans", today: "Vandag", tomorrow: "m&#244;re", cmdDelete: "Delete" },
-        sq: { code: "sq", name: "Albanian", today: "sot", tomorrow: "nes&#235;r", cmdDelete: "Delete" },
-        ar: { code: "ar", name: "Arabic", dir: "rtl", today: "&#1575;&#1604;&#1610;&#1608;&#1605;", tomorrow: "&#1594;&#1583;&#1575;", cmdDelete: "Delete" },
-        be: { code: "be", name: "Belarusian", today: "&#1089;&#1105;&#1085;&#1085;&#1103;", tomorrow: "&#1079;&#1072;&#1118;&#1090;&#1088;&#1072;", cmdDelete: "Delete" },
-        bs: { code: "bs", name: "Bosnian", today: "danas", tomorrow: "sutra", cmdDelete: "Delete" },
-        bg: { code: "bg", name: "Bulgarian", today: "&#1076;&#1085;&#1077;&#1089;", tomorrow: "&#1091;&#1090;&#1088;&#1077;", cmdDelete: "Delete" },
-        ca: { code: "ca", name: "Catalan", today: "avui", tomorrow: "dem&#224;", cmdDelete: "Delete" },
-        zh: { code: "zh", name: "Chinese", today: "&#20170;&#22825;", tomorrow: "&#26126;&#22825;", cmdDelete: "Delete" },
-        hr: { code: "hr", name: "Croatian", today: "danas", tomorrow: "sutra", cmdDelete: "Delete" },
-        cs: { code: "cs", name: "Czech", today: "dnes", tomorrow: "z&#237;tra", cmdDelete: "Delete" },
-        da: { code: "da", name: "Danish", today: "i dag", tomorrow: "i morgen", cmdDelete: "Delete" },
-        nl: { code: "nl", name: "Dutch", today: "vandaag", tomorrow: "morgen", cmdDelete: "Delete" },
-        et: { code: "et", name: "Estonian", today: "t&#228;na", tomorrow: "homme", cmdDelete: "Delete" },
-        fi: { code: "fi", name: "Finnish", today: "t&#228;n&#228;&#228;n", tomorrow: "huomenna", cmdDelete: "Delete" },
-        fr: { code: "fr", name: "French", today: "aujourd'hui", tomorrow: "demain", cmdDelete: "Delete" },
-        gl: { code: "gl", name: "Galician", today: "hoxe", tomorrow: "ma&#241;&#225;", cmdDelete: "Delete" },
-        de: { code: "de", name: "German", today: "heute", tomorrow: "morgen", cmdDelete: "Delete" },
-        el: { code: "el", name: "Greek", today: "&#963;&#942;&#956;&#949;&#961;&#945;", tomorrow: "&#945;&#973;&#961;&#953;&#959;", cmdDelete: "Delete" },
-        ht: { code: "ht", name: "Haitian", today: "jodi a", tomorrow: "demen", cmdDelete: "Delete" },
-        he: { code: "he", name: "Hebrew", dir: "rtl", today: "&#1492;&#1497;&#1493;&#1501;", tomorrow: "&#1502;&#1495;&#1512;", cmdDelete: "Delete" },
-        hi: { code: "hi", name: "Hindi", today: "&#2310;&#2332;", tomorrow: "&#2325;&#2354;", cmdDelete: "Delete" },
-        hu: { code: "hu", name: "Hungarian", today: "ma", tomorrow: "holnap", cmdDelete: "Delete" },
-        is: { code: "is", name: "Icelandic", today: "&#237; dag", tomorrow: "morgun", cmdDelete: "Delete" },
-        id: { code: "id", name: "Indonesian", today: "hari ini", tomorrow: "besok", cmdDelete: "Delete" },
-        ga: { code: "ga", name: "Irish", today: "l&#225; at&#225; inniu ann", tomorrow: "am&#225;rach", cmdDelete: "Delete" },
-        it: { code: "it", name: "Italian", today: "oggi", tomorrow: "domani", cmdDelete: "Delete" },
-        ja: { code: "ja", name: "Japanese", today: "&#20170;&#26085;", tomorrow: "&#26126;&#26085;", cmdDelete: "Delete" },
-        ko: { code: "ko", name: "Korean", today: "&#50724;&#45720;", tomorrow: "&#45236;&#51068;", cmdDelete: "Delete" },
-        lv: { code: "lv", name: "Latvian", today: "&#353;odien", tomorrow: "r&#299;t", cmdDelete: "Delete" },
-        lt: { code: "lt", name: "Lithuanian", today: "&#353;iandien", tomorrow: "rytoj", cmdDelete: "Delete" },
-        mk: { code: "mk", name: "Macedonian", today: "&#1076;&#1077;&#1085;&#1077;&#1089;", tomorrow: "&#1091;&#1090;&#1088;&#1077;", cmdDelete: "Delete" },
-        ms: { code: "ms", name: "Malay", today: "hari ini", tomorrow: "esok", cmdDelete: "Delete" },
-        mt: { code: "mt", name: "Maltese", today: "llum", tomorrow: "g&#295;ada", cmdDelete: "Delete" },
-        mo: { code: "mo", name: "Moldavian", today: "azi", tomorrow: "m&#226;ine", cmdDelete: "Delete" },
-        no: { code: "no", name: "Norwegian", today: "i dag", tomorrow: "i morgen", cmdDelete: "Delete" },
-        fa: { code: "fa", name: "Persian", dir: "rtl", today: "&#1575;&#1605;&#1585;&#1608;&#1586;", tomorrow: "&#1601;&#1585;&#1583;&#1575;", cmdDelete: "Delete" },
-        pl: { code: "pl", name: "Polish", today: "dzisiaj", tomorrow: "jutro", cmdDelete: "Delete" },
-        pt: { code: "pt", name: "Portuguese", today: "hoje", tomorrow: "amanh&#227;", cmdDelete: "Delete" },
-        ro: { code: "ro", name: "Romanian", today: "azi", tomorrow: "m&#226;ine", cmdDelete: "Delete" },
-        ru: { code: "ru", name: "Russian", today: "&#1089;&#1077;&#1075;&#1086;&#1076;&#1085;&#1103;", tomorrow: "&#1079;&#1072;&#1074;&#1090;&#1088;&#1072;", cmdDelete: "Delete" },
-        sr: { code: "sr", name: "Serbian", today: "&#1076;&#1072;&#1085;&#1072;&#1089;", tomorrow: "&#1089;&#1091;&#1090;&#1088;&#1072;", cmdDelete: "Delete" },
-        sk: { code: "sk", name: "Slovak", today: "dnes", tomorrow: "zajtra", cmdDelete: "Delete" },
-        sl: { code: "sl", name: "Slovenian", today: "danes", tomorrow: "jutri", cmdDelete: "Delete" },
-        es: { code: "es", name: "Spanish", today: "hoy", tomorrow: "ma&#241;ana", cmdDelete: "Delete" },
-        sw: { code: "sw", name: "Swahili", today: "leo", tomorrow: "kesho", cmdDelete: "Delete" },
-        sv: { code: "sv", name: "Swedish", today: "i dag", tomorrow: "i morgon", cmdDelete: "Delete" },
-        tl: { code: "tl", name: "Tagalog", today: "ngayon", tomorrow: "bukas", cmdDelete: "Delete" },
-        th: { code: "th", name: "Thai", today: "&#3623;&#3633;&#3609;&#3609;&#3637;&#3657;", tomorrow: "&#3623;&#3633;&#3609;&#3614;&#3619;&#3640;&#3656;&#3591;&#3609;&#3637;&#3657;", cmdDelete: "Delete" },
-        tr: { code: "tr", name: "Turkish", today: "bug&#252;n", tomorrow: "yar&#305;n", cmdDelete: "Delete" },
-        uk: { code: "uk", name: "Ukrainian", today: "&#1089;&#1100;&#1086;&#1075;&#1086;&#1076;&#1085;&#1110;", tomorrow: "&#1079;&#1072;&#1074;&#1090;&#1088;&#1072;", cmdDelete: "Delete" },
-        vi: { code: "vi", name: "Vietnamese", today: "h&#244;m nay", tomorrow: "mai", cmdDelete: "Delete" },
-        cy: { code: "cy", name: "Welsh", today: "heddiw", tomorrow: "yfory", cmdDelete: "Delete" },
-        yi: { code: "yi", name: "Yiddish", dir: "rtl", today: "&#1492;&#1497;&#1497;&#1463;&#1504;&#1496;", tomorrow: "&#1502;&#1488;&#1464;&#1512;&#1490;&#1503;", cmdDelete: "Delete" }
+        en: { code: "en", name: "English", today: "Today", tomorrow: "Tomorrow", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        af: { code: "af", name: "Afrikaans", today: "Vandag", tomorrow: "m&#244;re", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        sq: { code: "sq", name: "Albanian", today: "sot", tomorrow: "nes&#235;r", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ar: { code: "ar", name: "Arabic", dir: "rtl", today: "&#1575;&#1604;&#1610;&#1608;&#1605;", tomorrow: "&#1594;&#1583;&#1575;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        be: { code: "be", name: "Belarusian", today: "&#1089;&#1105;&#1085;&#1085;&#1103;", tomorrow: "&#1079;&#1072;&#1118;&#1090;&#1088;&#1072;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        bs: { code: "bs", name: "Bosnian", today: "danas", tomorrow: "sutra", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        bg: { code: "bg", name: "Bulgarian", today: "&#1076;&#1085;&#1077;&#1089;", tomorrow: "&#1091;&#1090;&#1088;&#1077;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ca: { code: "ca", name: "Catalan", today: "avui", tomorrow: "dem&#224;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        zh: { code: "zh", name: "Chinese", today: "&#20170;&#22825;", tomorrow: "&#26126;&#22825;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        hr: { code: "hr", name: "Croatian", today: "danas", tomorrow: "sutra", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        cs: { code: "cs", name: "Czech", today: "dnes", tomorrow: "z&#237;tra", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        da: { code: "da", name: "Danish", today: "i dag", tomorrow: "i morgen", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        nl: { code: "nl", name: "Dutch", today: "vandaag", tomorrow: "morgen", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        et: { code: "et", name: "Estonian", today: "t&#228;na", tomorrow: "homme", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        fi: { code: "fi", name: "Finnish", today: "t&#228;n&#228;&#228;n", tomorrow: "huomenna", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        fr: { code: "fr", name: "French", today: "aujourd'hui", tomorrow: "demain", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        gl: { code: "gl", name: "Galician", today: "hoxe", tomorrow: "ma&#241;&#225;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        de: { code: "de", name: "German", today: "heute", tomorrow: "morgen", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        el: { code: "el", name: "Greek", today: "&#963;&#942;&#956;&#949;&#961;&#945;", tomorrow: "&#945;&#973;&#961;&#953;&#959;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ht: { code: "ht", name: "Haitian", today: "jodi a", tomorrow: "demen", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        he: { code: "he", name: "Hebrew", dir: "rtl", today: "&#1492;&#1497;&#1493;&#1501;", tomorrow: "&#1502;&#1495;&#1512;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        hi: { code: "hi", name: "Hindi", today: "&#2310;&#2332;", tomorrow: "&#2325;&#2354;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        hu: { code: "hu", name: "Hungarian", today: "ma", tomorrow: "holnap", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        is: { code: "is", name: "Icelandic", today: "&#237; dag", tomorrow: "morgun", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        id: { code: "id", name: "Indonesian", today: "hari ini", tomorrow: "besok", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ga: { code: "ga", name: "Irish", today: "l&#225; at&#225; inniu ann", tomorrow: "am&#225;rach", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        it: { code: "it", name: "Italian", today: "oggi", tomorrow: "domani", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Aggiungi qualcosa di nuovo" },
+        ja: { code: "ja", name: "Japanese", today: "&#20170;&#26085;", tomorrow: "&#26126;&#26085;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ko: { code: "ko", name: "Korean", today: "&#50724;&#45720;", tomorrow: "&#45236;&#51068;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        lv: { code: "lv", name: "Latvian", today: "&#353;odien", tomorrow: "r&#299;t", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        lt: { code: "lt", name: "Lithuanian", today: "&#353;iandien", tomorrow: "rytoj", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        mk: { code: "mk", name: "Macedonian", today: "&#1076;&#1077;&#1085;&#1077;&#1089;", tomorrow: "&#1091;&#1090;&#1088;&#1077;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ms: { code: "ms", name: "Malay", today: "hari ini", tomorrow: "esok", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        mt: { code: "mt", name: "Maltese", today: "llum", tomorrow: "g&#295;ada", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        mo: { code: "mo", name: "Moldavian", today: "azi", tomorrow: "m&#226;ine", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        no: { code: "no", name: "Norwegian", today: "i dag", tomorrow: "i morgen", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        fa: { code: "fa", name: "Persian", dir: "rtl", today: "&#1575;&#1605;&#1585;&#1608;&#1586;", tomorrow: "&#1601;&#1585;&#1583;&#1575;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        pl: { code: "pl", name: "Polish", today: "dzisiaj", tomorrow: "jutro", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        pt: { code: "pt", name: "Portuguese", today: "hoje", tomorrow: "amanh&#227;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ro: { code: "ro", name: "Romanian", today: "azi", tomorrow: "m&#226;ine", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        ru: { code: "ru", name: "Russian", today: "&#1089;&#1077;&#1075;&#1086;&#1076;&#1085;&#1103;", tomorrow: "&#1079;&#1072;&#1074;&#1090;&#1088;&#1072;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        sr: { code: "sr", name: "Serbian", today: "&#1076;&#1072;&#1085;&#1072;&#1089;", tomorrow: "&#1089;&#1091;&#1090;&#1088;&#1072;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        sk: { code: "sk", name: "Slovak", today: "dnes", tomorrow: "zajtra", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        sl: { code: "sl", name: "Slovenian", today: "danes", tomorrow: "jutri", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        es: { code: "es", name: "Spanish", today: "hoy", tomorrow: "ma&#241;ana", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        sw: { code: "sw", name: "Swahili", today: "leo", tomorrow: "kesho", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        sv: { code: "sv", name: "Swedish", today: "i dag", tomorrow: "i morgon", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        tl: { code: "tl", name: "Tagalog", today: "ngayon", tomorrow: "bukas", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        th: { code: "th", name: "Thai", today: "&#3623;&#3633;&#3609;&#3609;&#3637;&#3657;", tomorrow: "&#3623;&#3633;&#3609;&#3614;&#3619;&#3640;&#3656;&#3591;&#3609;&#3637;&#3657;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        tr: { code: "tr", name: "Turkish", today: "bug&#252;n", tomorrow: "yar&#305;n", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        uk: { code: "uk", name: "Ukrainian", today: "&#1089;&#1100;&#1086;&#1075;&#1086;&#1076;&#1085;&#1110;", tomorrow: "&#1079;&#1072;&#1074;&#1090;&#1088;&#1072;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        vi: { code: "vi", name: "Vietnamese", today: "h&#244;m nay", tomorrow: "mai", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        cy: { code: "cy", name: "Welsh", today: "heddiw", tomorrow: "yfory", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" },
+        yi: { code: "yi", name: "Yiddish", dir: "rtl", today: "&#1492;&#1497;&#1497;&#1463;&#1504;&#1496;", tomorrow: "&#1502;&#1488;&#1464;&#1512;&#1490;&#1503;", cmdDelete: "Delete", cmdDone: "Done", cmdNotDone: "Not Done", cmdWrite: "Write something new" }
     };
 
     var module = {};
@@ -484,6 +484,7 @@
                 var lang = $(event.node).val();
                 setOption("selectedLanguageCode", lang);
                 this.set("selectedLanguageCode", lang);
+                this.set("lang", translations[lang]);
                 this.update();
             }
         });
